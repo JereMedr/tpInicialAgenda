1bb1f55b42af13a3563c6f124cb7841172595195
MarcoYu <marco.yu1995@gmail.com>
2020-09-15 21:57:08 -0300

    cambio en conexion

--- agenda/src/main/java/persistencia/conexion/Conexion.java
+++ agenda/src/main/java/persistencia/conexion/Conexion.java
@@ -1,57 +1,58 @@
 package persistencia.conexion;
-
+ 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import org.apache.log4j.Logger;
-
+ 
 public class Conexion 
 {
-	public static Conexion instancia;
-	private Connection connection;
-	private Logger log = Logger.getLogger(Conexion.class);	
-	
-	private Conexion()
-	{
-		try
-		{
-			Class.forName("com.mysql.jdbc.Driver"); // quitar si no es necesario
-			this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda?useSSL=false","root","root");//use ?useSSL=false Marco
-			this.connection.setAutoCommit(false);
-			log.info("ConexiÃ³n exitosa");
-		}
-		catch(Exception e)
-		{
-			log.error("ConexiÃ³n fallida", e);
-		}
-	}
-	
-	
-	public static Conexion getConexion()   
-	{								
-		if(instancia == null)
-		{
-			instancia = new Conexion();
-		}
-		return instancia;
-	}
-
-	public Connection getSQLConexion() 
-	{
-		return this.connection;
-	}
-	
-	public void cerrarConexion()
-	{
-		try 
-		{
-			this.connection.close();
-			log.info("Conexion cerrada");
-		}
-		catch (SQLException e) 
-		{
-			log.error("Error al cerrar la conexiÃ³n!", e);
-		}
-		instancia = null;
-	}
+    public static Conexion instancia;
+    private Connection connection;
+    private Logger log = Logger.getLogger(Conexion.class);  
+    
+    private Conexion()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver"); // comentario prueba branch no borrado
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda","root","root");
+            this.connection.setAutoCommit(false);
+            log.info("Conexión exitosa");
+        }
+        catch(Exception e)
+        {
+            log.error("Conexión fallida", e);
+        }
+    }
+    
+    
+    public static Conexion getConexion()   
+    {                               
+        if(instancia == null)
+        {
+            instancia = new Conexion();
+        }
+        return instancia;
+    }
+ 
+    public Connection getSQLConexion() 
+    {
+        return this.connection;
+    }
+    
+    public void cerrarConexion()
+    {
+        try 
+        {
+            this.connection.close();
+            log.info("Conexion cerrada");
+        }
+        catch (SQLException e) 
+        {
+            log.error("Error al cerrar la conexión!", e);
+        }
+        instancia = null;
+    }
 }
+ 
